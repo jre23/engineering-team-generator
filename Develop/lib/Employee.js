ad4c0414008a9f0007af32ef4a5646a6ee84487e
Joel Estrada <jre.estrada@gmail.com>
2020-11-08 16:16:04 -0800

    changed from using class expression to class declaration in Employee.js and Engineer.js

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,5 +1,5 @@
 // TODO: Write code to define and export the Employee class
-const Employee = class {
+class Employee {
     constructor(name, id, email) {
         this.name = name;
         this.id = id;
